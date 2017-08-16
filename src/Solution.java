6781d880be940ad19fcacf1189372ecbd808efed
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-16 17:00:20 -0400

    first solution

--- src/Solution.java
+++ src/Solution.java
@@ -7,6 +7,17 @@ public class Solution {
         /* Enter your code here. Read input from STDIN.
         Print output to STDOUT. Your class should be named Solution. */
 
-        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        String[] ar = new String[N];
+        for (int i = 0; i < N; i++) {
+            ar[i] = sc.nextLine();
+        }
+
+
+        System.out.println(ar[2]);
+
     }
 }
