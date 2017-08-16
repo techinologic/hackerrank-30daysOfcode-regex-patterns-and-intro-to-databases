44ee53650c6ba1ba402e26005344c5eeafc5e5fc
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-16 17:55:54 -0400

    working code

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Solution {
+class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN.
         Print output to STDOUT. Your class should be named Solution. */
 
+        /*
+        1. scan N = number of entries
+        2. store entries intp a string array
+        3. print entries that has @gmail.com in them
+         */
+
         Scanner sc = new Scanner(System.in);
+
+        Pattern p = Pattern.compile("\\p{Alpha}+");
+
         int N = sc.nextInt();
         sc.nextLine();
 
@@ -16,8 +27,41 @@ public class Solution {
             ar[i] = sc.nextLine();
         }
 
+        String myString;
+        ArrayList<String> L = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            myString = ar[i];
+            Matcher m = p.matcher(myString);
 
-        System.out.println(ar[2]);
+            if (m.find() && myString.contains("@gmail.com")) {
+                L.add(m.group());
+            }
+        }
+
+        L.sort(String::compareToIgnoreCase);
+
+        for (String s :
+                L) {
+            System.out.println(s);
+        }
 
     }
 }
+
+/*
+
+6
+riya riya@gmail.com
+julia julia@julia.me
+julia sjulia@gmail.com
+julia julia@gmail.com
+samantha samantha@gmail.com
+tanya tanya@gmail.com
+
+
+
+
+
+
+
+ */
